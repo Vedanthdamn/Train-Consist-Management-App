601a82414cf6d4ad716b0002a5919c5f45b93c2b
Vedanthdamn <vedanthdama@gmail.com>
2026-04-09 00:18:25 +0530

    feat: UC5 - Preserve insertion order using LinkedHashSet

---
+++ src/UseCase5TrainConsistMgmt.java
@@ -0,0 +1,29 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class UseCase5TrainConsistMgmt {
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println(" UC5 - Preserve Insertion Order of Bogies ");
+        System.out.println("======================================\n");
+
+        Set<String> trainFormation = new LinkedHashSet<>();
+
+        trainFormation.add("Engine");
+        trainFormation.add("Sleeper");
+        trainFormation.add("Cargo");
+        trainFormation.add("Guard");
+
+        trainFormation.add("Sleeper");
+
+        System.out.println("Final Train Formation:");
+        System.out.println(trainFormation);
+
+        System.out.println("\nNote:");
+        System.out.println("LinkedHashSet preserves insertion order and removes duplicates automatically.");
+
+        System.out.println("\nUC5 formation setup completed...");
+    }
+}
